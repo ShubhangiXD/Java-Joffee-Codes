cfad37b6879c52bdb2691db52448aa4f829db515
Renyxx <d.shubhangi1510@gmail.com>
2023-08-27 09:04:48 +0530

    new file:   C1_defaultconstructor.java

---
+++ C1_defaultconstructor.java
@@ -0,0 +1,9 @@
+public class C1_defaultconstructor {
+    C1_defaultconstructor() {
+        System.out.println("Default constructor called");
+    }
+
+    public static void main(String[] args) {
+        C1_defaultconstructor obj = new C1_defaultconstructor();
+    }
+}
---
+++ C2_studentconstructor.java
@@ -0,0 +1,21 @@
+public class C2_studentconstructor {
+    int rollno;
+    String name;
+
+    C2_studentconstructor(int rollno, String name) {
+        this.rollno = rollno;
+        this.name = name;
+        //'this' keyword refers to the current values in a method/constructor (to eliminate confusion)
+    }
+
+    void display() {
+        System.out.println(rollno + " " + name + " ");
+    }
+
+    public static void main(String[] args) {
+        C2_studentconstructor s1 = new C2_studentconstructor();
+        C2_studentconstructor s2 = new C2_studentconstructor();
+        s1.display();
+        s2.display();
+    }
+}
