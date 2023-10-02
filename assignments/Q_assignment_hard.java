a686e3903faf9565f73aba449837e7a91537a1a3
Renyxx <d.shubhangi1510@gmail.com>
2023-10-02 10:56:25 +0530

    new file:   assignments/Q_assignment_strings.java

--- assignments/Q_assignment_hard.java
+++ assignments/Q_assignment_hard.java
@@ -1,5 +1,6 @@
 package assignments;
 //wap to check whether str and str1 are anagrams of each other
+//[PS: this is an extra credit question]
 public class Q_assignment_hard {
     public static void main(String[] args) {
         String str = "anagram";
---
+++ assignments/Q_assignment_strings.java
@@ -0,0 +1,11 @@
+package assignments;
+
+//write a program to input a string and a number n and extract the n-th character from the string if present, otherwise print "Index not found".
+public class Q_assignment_strings {
+    public static void main(String[] args) {
+        //take user inputs here
+    }
+    void findCharacter(String str, int n){
+        //write code here to check whether n is within bounds, if yes, print character, if no, print "Index not found".
+    }
+}
